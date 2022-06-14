17af2c79404eba2dffd400abdb34d00f52a01ab3
raulverap <raul.vera.portanda@gmail.com>
2022-06-13 21:44:57 -0400

    cambio de colore

--- app/src/main/java/com/example/tablamatematica/MainActivity.java
+++ app/src/main/java/com/example/tablamatematica/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.tablamatematica;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button bsumar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void siEsta(View v){
+        Intent it = new Intent(this,pantalla2.class);
+        startActivity(it);
+
+    }
+
+    public void noEsta(View v){
+
     }
 }
