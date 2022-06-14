ceb5f2f0f6752632330543da1dcfc7fc9c04957d
raulverap <raul.vera.portanda@gmail.com>
2022-06-13 21:08:13 -0400

    primera version, parte grafica

---
+++ app/src/main/java/com/example/tablamatematica/Pantalla5.java
@@ -0,0 +1,13 @@
+package com.example.tablamatematica;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Pantalla5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla5);
+    }
+}
